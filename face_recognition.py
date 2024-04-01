b592dead748f6ef520b2cfa2693407563ea71467
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-04-01 12:31:59 -0600

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -1,8 +1,7 @@
 import cv2
 import time
-import numpy as np
+import base64
 import requests
-import json
 import pywhatkit
 from pymongo import MongoClient
 
@@ -23,6 +22,7 @@ def insert_document(response, age):
     collection = db['datos']
 
     # Crea el documento a insertar
+    print(document)
     document = {
         'response': response,
         'age': age
@@ -37,80 +37,60 @@ def insert_document(response, age):
 
 # End DB Connection
 
-
 client_id = 'hkRmyxdbrq4y84fjmUOFR0UD'
 client_secret = '40UggdZZNOa8E2pKvLEuEETQoXDZkjOMw0iFtwZ3Ii2a3sDs'
-
 net = cv2.dnn.readNetFromCaffe("deploy.prototxt", "res10_300x300_ssd_iter_140000.caffemodel")
+cap = cv2.VideoCapture(0)
+frame_count = 0
 
-cap = cv2.VideoCapture(0) 
-
-def age_detection(img):
-    
-    params = {'url': 'https://labs.everypixel.com/static/i/estest_sample3.jpg'}
-    response = requests.get('https://api.everypixel.com/v1/faces', params=params, auth=(client_id, client_secret)).json()
-    
-    print(response)
+def age_detection(filename):
+    with open(filename, 'rb') as file:
+        data = {'data': file}
+        response = requests.post('https://api.everypixel.com/v1/faces', files=data, auth=(client_id, client_secret)).json()
+   
     if response['status'] == 'ok':
         for face in response['faces']:
             age = face['age']
             print(f"Estimated age: {age}")
-            insert_document(response, age)
-            # wa_messege()
-
+            if(age<30):
+                print("underage ")
+                message = f'there is a underage person in the kitchen '
+                wa_messege(message)
+                insert_document(response, age)
     else:
         print("No faces detected.")
 
-frame_count = 0
-
-def wa_messege():
+def wa_messege(message):
     try:
-    
-        # sending message to receiver
-        # using pywhatkit
-        pywhatkit.sendwhatmsg("+5213326176382", 
-                                "alerta de incendio", 
-                                1, 1)
+        # sending message to receiver using pywhatkit
+        current_time = time.localtime()
+        pywhatkit.sendwhatmsg("+5213323651326",
+                              message,
+                              current_time.tm_hour,
+                              current_time.tm_min + 1)
         print("Successfully Sent!")
-        
     except:
-    
-        # handling exception 
-        # and printing error message
+        # handling exception and printing error message
         print("An Unexpected Error!")
 
 while True:
     ret, frame = cap.read()
-
-
     resized_frame = cv2.resize(frame, (300, 300))
 
     blob = cv2.dnn.blobFromImage(resized_frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
-
     net.setInput(blob)
-
     detections = net.forward()
-
     person_detected = False
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
-        if confidence > 0.5: 
+        if confidence > 0.5:
             person_detected = True
             break
-
     if person_detected:
         print("Person detected in the image!")
         frame_count += 1
         filename = f"captured_image_{frame_count}.jpg"
         cv2.imwrite(filename, frame)
-        print(f"Image saved as {filename}")
-        age_detection(resized_frame)
-        # wa_messege()
-    else:
-        print("No person detected in the image.")
-
-        
+        age_detection(filename)
 
     time.sleep(5)
-
-
